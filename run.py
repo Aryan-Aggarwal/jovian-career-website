57f44f93460abecb204acdab692aad4cf7211aa2
Aryan-Aggarwal <89564931+Aryan-Aggarwal@users.noreply.github.com>
2023-06-10 21:03:28 +0000

    added css and html files

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  a =90
-  return "This is the number {}".format(a)
+  return render_template("home.html")
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug = True)
